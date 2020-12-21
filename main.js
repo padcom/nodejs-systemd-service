311874d64810a99011811e56ed40432a16eecb8e
Matthias Hryniszak <mhryn@softserveinc.com>
2020-12-22 00:46:59 +0100

    Added option to set the port the app is listening on

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello, world! in ' + process.env.NODE_ENV + '\n')
+  res.send('Hello, world! in ' + process.env.NODE_ENV + 'with hidden message ' + process.env.MESSAGE + '\n')
   console.log('Hello, world! in ', process.env.NODE_ENV)
   console.log('Hidden message', process.env.MESSAGE)
 })
@@ -16,6 +16,6 @@ app.get('/kill', (req, res) => {
   process.exit()
 })
 
-app.listen(3000, () => {
-  console.log('Example application listening on port 3000')
+const listener = app.listen(process.env.PORT || 3000, (...args) => {
+  console.log('Example application listening on port', listener.address().port)
 })
