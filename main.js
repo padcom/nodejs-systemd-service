ecf04f2543d7ac5c6fb2e8d4f1e82741794f916f
Matthias Hryniszak <mhryn@softserveinc.com>
2020-12-22 01:52:29 +0100

    Added systemd notification that the application is up, running and ready to receive connections

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const express = require('express')
+const notify = require('sd-notify')
 
 const app = express()
 
@@ -18,4 +19,19 @@ app.get('/kill', (req, res) => {
 
 const listener = app.listen(process.env.PORT || 3000, (...args) => {
   console.log('Example application listening on port', listener.address().port)
+
+  // The following line tells systemd that the server has reached the point where
+  // it is ready to accept connections and thus the service is fully up and running.
+  //
+  // To utilize this you need to set the [Service] Type=notify which is not possible
+  // by default via node-deb overrides. Therefore in this repository there is the
+  // nodejs-systemd-example.service file which is identical to the one used by default
+  // by node-deb but chnages the aforementioned Type to notify. The template override
+  // is in package.json (.node_deb.templates.systemd_service)
+  //
+  // In addition to the required dependencies the libsystemd-dev package is required
+  // so that the native binaries for sd-notify can be built
+  // (see package.json, .node_deb.dependencies)
+
+  notify.ready()
 })
