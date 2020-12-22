aa1c4e9dbacf0160fb73c969fb4fc51bbeba1549
Matthias Hryniszak <mhryn@softserveinc.com>
2020-12-22 02:13:17 +0100

    Added information on startup timeout and watchdog mode

--- main.js
+++ main.js
@@ -25,13 +25,24 @@ const listener = app.listen(process.env.PORT || 3000, (...args) => {
   //
   // To utilize this you need to set the [Service] Type=notify which is not possible
   // by default via node-deb overrides. Therefore in this repository there is the
-  // nodejs-systemd-example.service file which is identical to the one used by default
-  // by node-deb but chnages the aforementioned Type to notify. The template override
-  // is in package.json (.node_deb.templates.systemd_service)
+  // nodejs-systemd-example.service file which is almost identical to the one used by
+  // default by node-deb but chnages the aforementioned Type to notify.
+  // The template override is in package.json (.node_deb.templates.systemd_service).
+  // Another useful feature is the timeout to start. In this example the overriden
+  // template contains the [Service] TimeoutStartSec=5 which means that if the service
+  // will not start in 30 seconds something's wrong.
+  //
+  // Additionally, if the service gets stuck for some reason it should probably be
+  // restarted. For that we have the [Service] WatchdogSec=3 setting in the service
+  // description file. There are two ways to use it:
+  // - setTimeout/setInterval and call the notify.watchdog() method
+  // - notify.startWatchdogMode(ms) which will do the above automatically (used in
+  //   this example)
   //
   // In addition to the required dependencies the libsystemd-dev package is required
   // so that the native binaries for sd-notify can be built
   // (see package.json, .node_deb.dependencies)
 
   notify.ready()
+  notify.startWatchdogMode(2800)
 })
